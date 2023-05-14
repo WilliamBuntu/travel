d58b2bdb5f65a0378fcc21a65eb36461793338ef
Buntu William <williambuntu1@gmail.com>
2023-05-15 01:22:46 +0200

    t commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <div>
-      App
+      <>
+      <Navbar/>
+
+
+      <Home/>
+      <Main/>
+      <Footer/>
+
+
+      </>
+
+
+
     </div>
   )
 }
